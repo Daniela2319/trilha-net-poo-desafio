7f970c60060fb6792965f3ff7545d85de020bc20
Daniela2319 <frankvt04@gmail.com>
2025-06-03 11:27:44 -0300

    Adicionando codigo nos campos que faltavam e classe menuNokia

---
+++ Models/MenuNokia.cs
@@ -0,0 +1,81 @@
+using DesafioPOO.Models;
+
+namespace trilha_net_poo_desafio.Models
+{
+    public class MenuNokia : Nokia
+    {
+   
+        public MenuNokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public void ExibirMenu()
+        {
+            ImprimirCabecalho();
+            UsarDoWhile();
+        }
+
+
+        public void ImprimirCabecalho()
+        {
+            Console.WriteLine("Bem-vindo ao Menu Nokia!");
+            Console.WriteLine("-------------------------");
+        }
+        public void ImprimirRodape()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Obrigado por usar o Menu Nokia!");
+        }
+        public int LerOpcaoUsuario()
+        {
+            System.Console.WriteLine("Digite a opção desejada:");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            return opcao;
+        }
+
+        public void ImprimirMenu()
+        {
+            Console.WriteLine("1 - Ligar");
+            Console.WriteLine("2 - Receber Ligação");
+            Console.WriteLine("3 - Instalar Aplicativo");
+            Console.WriteLine("0 - Sair");
+        }
+        public void ExecutarOpcoesMenu(int opcao)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Ligar();
+                    break;
+                case 2:
+                    ReceberLigacao();
+                    break;
+                case 3:
+                    Console.Write("Digite o nome do aplicativo: ");
+                    string app = Console.ReadLine();
+                    InstalarAplicativo(app);
+                    break;
+                case 0:
+                    Console.WriteLine("Saindo...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+
+        public void UsarDoWhile()
+        {
+            string opcao;
+            do
+            {
+                ImprimirCabecalho();
+                ImprimirMenu();
+                opcao = Console.ReadLine();
+                ExecutarOpcoesMenu(Convert.ToInt32(opcao));
+                ImprimirRodape();
+            } while (opcao != "0");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
+using trilha_net_poo_desafio.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+MenuNokia menuNokia = new MenuNokia("11999999999", "Nokia 6.1", "1234567890", 64);
+menuNokia.ExibirMenu();
+
+System.Console.WriteLine("-----------------------------------");
+
+Smartphone iphone = new Iphone("11988888888", "iPhone 12", "0987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
